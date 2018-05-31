bc7106d0f9e911f56e486cf42d41cb708468bb35
cirnicirno <cirnicirno@users.noreply.github.com>
2018-05-31 12:28:53 +0300

    Разбил код на отдельные функции

--- src/main/java/lab/Lab3.java
+++ src/main/java/lab/Lab3.java
@@ -5,7 +5,7 @@ public class Lab3 {
     public static void main(String[] args){
 
         DoubleSwap doubleSwap = new DoubleSwap();
-        doubleSwap.swap();
+        doubleSwap.input();
 
     }
 
